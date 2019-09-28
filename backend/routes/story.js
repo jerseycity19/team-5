3514b57e5c2cb6eb53d0ed3b2df4813f1bde0a83
Jake Billings <jake@jakebillings.com>
2019-09-28 05:42:59 -0400

    Add: geocoding of location as they come in

--- backend/routes/story.js
+++ backend/routes/story.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Story = require('../models/Story')
+const geocoder = require('../facades/geocoder')
 
 // This grabs all the stories
 router.get('/', (req, res) => {
@@ -9,29 +10,47 @@ router.get('/', (req, res) => {
       res.status(500).send({ 'success': false, msg: 'Problem accessing database' })
     }
 
-    res.send(stories)
+    res.json(stories)
   })
 })
 
 // This writes a story to the database
 router.post('/', (req, res) => {
-  let now = Date()
-  let newStory = new Story({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    date: now,
-    city: req.body.city,
-    state: req.body.state,
-    content: req.body.content,
-  })
-
-  newStory.save((err) => {
+  geocoder.geocode(`${req.body.city}, ${req.body.state}`, (err, geocodeResult) => {
     if (err) {
-      console.log(err)
-      res.status(422).json({ 'success': false, msg: 'Could not save into database' })
-    } else {
-      res.send({ 'success': true })
+      res.status(500).json({ success: false, msg: 'Geocoder failed' })
     }
+
+    console.log(err, geocodeResult)
+
+    let latitude = null
+    let longitude = null
+
+    if (geocodeResult.length > 0) {
+      latitude = geocodeResult[0].latitude
+      longitude = geocodeResult[0].longitude
+    }
+
+    let now = Date()
+    let newStory = new Story({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      date: now,
+      city: req.body.city,
+      state: req.body.state,
+      content: req.body.content,
+      latitude,
+      longitude,
+    })
+
+    newStory.save((err) => {
+      if (err) {
+        console.log(err)
+        res.status(422).json({ 'success': false, msg: 'Could not save into database' })
+      } else {
+        res.json({ 'success': true })
+      }
+    })
   })
 })
 
