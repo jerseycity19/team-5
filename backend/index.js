42b56ed4cd1a6544e4b5cd3e59573804abb00762
Jake Billings <jake@jakebillings.com>
2019-09-27 21:29:16 -0400

    Add: dummy parse

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require("body-parser")
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
     console.log(req.body)
     return res.send({ "success": true });
 })
+
+
+app.post("/twilio-webhook", (req, res) => {
+    console.log(req.body)
+    return res.send({ "success": true });
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
