744d0b59b89d2aae31bf957fc8d37f15096712d9
Jake Billings <jake@jakebillings.com>
2019-09-28 09:58:10 -0400

    Add: link to the chat room from the chat room list

--- frontend/src/layout/components/ChatRoomList.js
+++ frontend/src/layout/components/ChatRoomList.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Collapse from '@material-ui/core/Collapse';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import Button from '@material-ui/core/Button';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import ListSubheader from '@material-ui/core/ListSubheader'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import Collapse from '@material-ui/core/Collapse'
+import ExpandLess from '@material-ui/icons/ExpandLess'
+import ExpandMore from '@material-ui/icons/ExpandMore'
+import Button from '@material-ui/core/Button'
+import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -18,41 +19,41 @@ const useStyles = makeStyles(theme => ({
   nested: {
     paddingLeft: theme.spacing(4),
   },
-}));
+}))
 
-export default function ChatRoomList() {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+export default function ChatRoomList () {
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(false)
 
   const handleClick = () => {
-    setOpen(!open);
-  };
+    setOpen(!open)
+  }
 
   return (
     <div>
       <br></br>
-    <List
-      component="nav"
-      aria-labelledby="nested-list-subheader"
-      subheader={
-        <ListSubheader component="div" id="nested-list-subheader">
-          Chat Rooms
-        </ListSubheader>
-      }
-      className={classes.root}
-    >
-      <ListItem button onClick={handleClick}>
-        <ListItemText primary="Louisville, Kentucky" />
-        {open ? <ExpandLess /> : <ExpandMore />}
-      </ListItem>
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          <ListItem button className={classes.nested}>
-            <Button color="primary">Join Chat Room</Button>>
-          </ListItem>
-        </List>
-      </Collapse>
-    </List>
+      <List
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        subheader={
+          <ListSubheader component="div" id="nested-list-subheader">
+            Chat Rooms
+          </ListSubheader>
+        }
+        className={classes.root}
+      >
+        <ListItem button onClick={handleClick}>
+          <ListItemText primary="Louisville, Kentucky"/>
+          {open ? <ExpandLess/> : <ExpandMore/>}
+        </ListItem>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItem button className={classes.nested}>
+              <Link to="/chat"><Button color="primary">Join Chat Room</Button></Link>
+            </ListItem>
+          </List>
+        </Collapse>
+      </List>
     </div>
   )
 }
