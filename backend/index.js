65c774c5e5d1bc76c3453ae9c65144c69f8acdce
Kyle Respicio <kyle.b.respicio@gmail.com>
2019-09-27 20:33:29 -0400

    Created the mininmum backend

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get("/", (req, res) => {
+    return res.send({ "success": true });
+})
