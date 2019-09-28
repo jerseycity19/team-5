10b400c679d4ce3d100d92bdd1ee02d138ab4c81
Jake Billings <jake@jakebillings.com>
2019-09-27 21:40:31 -0400

    Tweak: urlencoded parser

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser")
 
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
     console.log(req.body)
