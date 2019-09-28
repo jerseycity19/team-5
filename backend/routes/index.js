aba0cf4b54c2a520f894b82cb809073217d32a4d
Jake Billings <jake@jakebillings.com>
2019-09-28 03:31:55 -0400

    Fix: typo in routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
 
 const twilioWebhookRouter = require('./twilioWebhook')
-const storyRouter = require('./storyRouter')
+const storyRouter = require('./story')
 
 const router = Router()
 
