bd351305eed1d8d89f92097575da1f7968d5f51f
Jake Billings <jake@jakebillings.com>
2019-09-28 02:50:00 -0400

    Add: a get all route to the story router

--- backend/routes/story.js
+++ backend/routes/story.js
@@ -1,33 +1,38 @@
-const express = require("express");
-const router = express.Router();
-const Story = require("../models/Story")
+const express = require('express')
+const router = express.Router()
+const Story = require('../models/Story')
 
 // This grabs all the stories
-router.get("/allStories", (req, res) => {
+router.get('/', (req, res) => {
+  Story.find({}, (err, stories) => {
+    if (err) {
+      res.status(500).send({ 'success': false, msg: 'Problem accessing database' })
+    }
 
+    res.send(stories)
+  })
 })
 
 // This writes a story to the database
-router.post("/writeStory", (req, res) => {
-    let now = Date();
-    let newStory = new Story({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        date: now,
-        city: req.body.city,
-        state: req.body.state,
-        content: req.body.content
-    })
+router.post('/', (req, res) => {
+  let now = Date()
+  let newStory = new Story({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    date: now,
+    city: req.body.city,
+    state: req.body.state,
+    content: req.body.content,
+  })
 
-    newStory.save((err) => {
-        if (err) {
-            console.log(err);
-            res.json({ "success": false, msg: "Could not save into database" })
-        }
-        else {
-            res.json({ "success": true })
-        }
-    })
+  newStory.save((err) => {
+    if (err) {
+      console.log(err)
+      res.status(422).json({ 'success': false, msg: 'Could not save into database' })
+    } else {
+      res.send({ 'success': true })
+    }
+  })
 })
 
-module.exports = router;
+module.exports = router
