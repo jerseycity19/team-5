7437e68b023edc0ab6db19dc122b2de1a283d7a3
Jake Billings <jake@jakebillings.com>
2019-09-27 21:13:45 -0400

    Add: console.log req.body

--- backend/index.js
+++ backend/index.js
@@ -5,5 +5,6 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.get("/", (req, res) => {
+    console.log(req.body)
     return res.send({ "success": true });
 })
